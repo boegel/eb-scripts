3bd424e66acf8eb3d19c48724e443cc799d1a086
Kenneth Hoste <kenneth.hoste@ugent.be>
2018-08-11 16:14:16 +0200

    add another fluke pattern for 'pip install' failing with time out

--- pr_check.py
+++ pr_check.py
@@ -322,6 +322,7 @@ def is_fluke(job_log_txt):
         r"No output has been received in the last [0-9]*m[0-9]*s, this potentially indicates a stalled build",
         r"curl.*SSL read: error",
         r"A TLS packet with unexpected length was received",
+        r"ReadTimeoutError:.*Read timed out",
     ]
     fluke = False
     for pattern in fluke_patterns:
