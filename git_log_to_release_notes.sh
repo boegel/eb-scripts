7ed332f0d3a045d0978355038e7ee5a4bee8b4c1
Kenneth Hoste <kenneth.hoste@ugent.be>
2019-05-19 19:15:51 +0200

    fix 'sed' command in git_log_to_release_notes.sh (fixes #4)

--- git_log_to_release_notes.sh
+++ git_log_to_release_notes.sh
@@ -1,1 +1,1 @@
- git log | grep 'Merge pull' -A 5 | egrep -v "^commit|^Author|^Merge: " | sed 's/^--/@/g' | tr '\n' ' ' | tr '@' '\n' | sed 's/.*Merge pull request #\([^ ]*\).* from [^ ]*[ ]*\(.*\)/\2 (#\1)/g'
+ git log | grep 'Merge pull' -A 5 | egrep -v "^commit|^Author|^Merge: " | sed 's/^--/@/g' | tr '\n' ' ' | tr '@' '\n' | sed 's/.*Merge pull request #\([^ ]*\)\s* from [^ ]*[ ]*\(.*\)/\2 (#\1)/g'
